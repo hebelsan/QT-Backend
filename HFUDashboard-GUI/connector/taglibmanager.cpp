e5ddb35b506a276c16d106564bcecbc7264bde32
alex <alex.example@web.de>
2018-01-17 16:59:51 +0100

    fix

--- HFUDashboard-GUI/connector/taglibmanager.cpp
+++ HFUDashboard-GUI/connector/taglibmanager.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include "connector/connector.h"
 #include "colorimageprovider.h"
+#include "QVector"
 
 //#include <sys/types.h>
 //#include <sys/stat.h>
@@ -126,37 +127,56 @@ void TaglibManager::setFiles(char * files) {
 void TaglibManager::deliverUsbViewInformation(char *files) {
     Connector *creader = Connector::getInstance();
     ColorImageProvider *musicCoverConnector = ColorImageProvider::getInstance();
-    // QVariantList informationList;
-    QStringList filesInString;
+    QVariantList informationList;
+    QVector <std::string> filesInString;
     QStringList titelList;
     QStringList artistList;
     QList<QVariant> titleLengthSeconds;
     //QHash<QString, QImage> titleCovers;
-    //QStringList coverIds;
+    QStringList coverIds;
 
     std::string filesString(files);
-    std::string delimiter = "////";
+    std::string delimiter1 = "////";
+    std::string delimiter2 = "||||";
 
     // get every entry of string
     size_t pos = 0;
     std::string fileName;
-    while ((pos = filesString.find(delimiter)) != std::string::npos)
+    while ((pos = filesString.find(delimiter1)) != std::string::npos)
     {
         fileName = filesString.substr(0, pos);
-        filesInString.append(QString::fromStdString(fileName));
-        filesString.erase(0, pos + delimiter.length());
+        filesInString.push_back(fileName);
+        filesString.erase(0, pos + delimiter1.length());
     }
 
 
-    for (int i = 0; i < filesInString.length; i++) {
-        qInfo(filesInString[i].toLatin1());
+    for (int i = 0; i < filesInString.length(); i++) {
+        qInfo(filesInString[i].c_str());
+    }
+
+    for (int i = 0; i < filesInString.size(); i++) {
+        QStringList info;
+        while ((pos = filesInString[i].find(delimiter2)) != std::string::npos)
+        {
+            fileName = filesInString[i].substr(0, pos);
+            info.append(QString::fromStdString(fileName));
+            filesInString[i].erase(0, pos + delimiter1.length());
+        }
+        titelList.append(info.at(0));
+        artistList.append(info.at(1));
+        titleLengthSeconds.append(info.at(2).toInt());
+        if (info.at(1).isEmpty()) {
+            coverIds.append("Music");
+        } else {
+            coverIds.append("Dir");
+        }
     }
 
     // musicCoverConnector->setCovers(titleCovers);
 
-//    informationList << QVariant(titelList);
-//    informationList << QVariant(artistList);
-//    informationList << QVariant(titleLengthSeconds);
-//    informationList << QVariant(coverIds);
-//    emit creader -> sendNewMusicList(tagList);
+    informationList << QVariant(titelList);
+    informationList << QVariant(artistList);
+    informationList << QVariant(titleLengthSeconds);
+    informationList << QVariant(coverIds);
+    emit creader -> sendNewMusicList(informationList);
 }
